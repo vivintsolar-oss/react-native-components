a755472fac7989f2b014bcf8c1baaa640096edd1
tcasey <hello@tcasey.me>
2018-01-17 16:46:36 -0700

    refactor(VSInput): moves state changes into render - adds password state - uses native-vs-constants - uses native-vs-icons

--- packages/VSInput/index.js
+++ packages/VSInput/index.js
@@ -1,57 +1,142 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  TouchableWithoutFeedback,
-} from 'react-native';
-import AlertFillIcon from './AlertFillIcon';
-import InfoFillIcon from './InfoFillIcon';
+import { StyleSheet, Text, TextInput, View, TouchableWithoutFeedback } from 'react-native';
+import VSSvg from '@vivintsolar-oss/native-vs-icons';
+import Colors from '@vivintsolar-oss/native-vs-constants/Colors';
+import Touchable from '@vivintsolar-oss/native-vs-touchable';
 
-export default class VSInput extends React.Component {
-  render() {
-    const warning = this.props.error;
-    const { info } = this.props;
-    const myProps = {
-      style: warning
-        ? [ styles.input, styles.inputWarn ]
-        : [ styles.input, styles.inputNorm ],
+export default class VSInput extends Component {
+  constructor(props) {
+    super(props);
+    this.togglePw = this.togglePw.bind(this);
+    this.state = {
+      showPw: false,
     };
+  }
 
-    if (!this.props.reference) {
+  togglePw() {
+    return this.setState({ showPw: !this.state.showPw });
+  }
+
+  borderState() {
+    if (this.props.error) {
+      return Colors.ERROR;
+    }
+
+    return Colors.BORDER_GRAY;
+  }
+
+  labelState() {
+    if (this.props.error) {
+      return Colors.ERROR;
+    }
+
+    return Colors.BLACK;
+  }
+
+  render() {
+    const styles = StyleSheet.create({
+      labelConainer: {
+        flexDirection: 'row',
+        width: '100%',
+        marginTop: 8,
+      },
+      label: {
+        fontSize: 12,
+        color: this.labelState(),
+      },
+      input: {
+        width: '100%',
+        height: 48,
+        borderWidth: 1,
+        borderRadius: 4,
+        marginBottom: 8,
+        marginTop: 8,
+        padding: 8,
+        borderColor: this.borderState(),
+      },
+      icon: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 40,
+        height: 48,
+        position: 'absolute',
+        top: 8,
+        right: 0,
+      },
+    });
+
+    const { error, reference, form, index, label, toggle, info, password } = this.props;
+    const { showPw } = this.state;
+    const warning = error;
+    const myProps = {
+      style: styles.input,
+    };
+    let PwIcon = VSSvg.NoViewFillIcon;
+    let LabelIcon = VSSvg.AlertFillIcon;
+    let LabelIconColor = Colors.ERROR;
+    let IconWrapper = null;
+
+    if (!reference) {
       myProps.reference = (input) => {
-        this.props.form[this.props.index] = input;
+        form[index] = input;
       };
+    }
+
+    if (info) {
+      LabelIcon = VSSvg.InfoFillIcon;
+      LabelIconColor = Colors.ORANGE;
+      IconWrapper = <Touchable onPress={ toggle } ><LabelIcon color={ LabelIconColor } width={ 16 } height={ 16 } /></Touchable>;
+    }
+
+    if (warning) {
+      LabelIcon = VSSvg.AlertFillIcon;
+      LabelIconColor = Colors.ERROR;
+      IconWrapper = <LabelIcon color={ LabelIconColor } width={ 16 } height={ 16 } />;
+    }
+
+    if (password) {
+      if (showPw) {
+        PwIcon = VSSvg.EyeFillIcon;
+      }
+      myProps.secureTextEntry = !showPw;
     }
 
     return (
       <View style={ styles.container }>
         <View style={ styles.labelConainer }>
-          <Text style={ warning ? styles.errLabel : styles.label }>
-            {this.props.label}{' '}
-          </Text>
-          {warning ?
-            <AlertFillIcon color={ '#E74C3C' } width={ 16 } height={ 16 } />
-           :
-            <View style={ { width: 16, height: 16 } } />
-          }
-          {info && !warning ?
-            <TouchableWithoutFeedback onPress={ this.props.toggle }>
-              <InfoFillIcon color={ '#FF8200' } width={ 16 } height={ 16 } />
-            </TouchableWithoutFeedback>
-           :
-            <View style={ { width: 16, height: 16 } } />
-          }
+          <Text style={ styles.label }>{ label }  </Text>
+          { IconWrapper }
         </View>
-        <TextInput
-          { ...myProps }
-          { ...this.props }
-          ref={ this.props.reference }
-          underlineColorAndroid="rgba(0,0,0,0)"
-          autoCorrect={ false }
-        />
+        {
+          password
+            ? <View>
+              <TextInput
+                { ...myProps }
+                { ...this.props }
+                ref={ reference }
+                underlineColorAndroid="rgba(0,0,0,0)"
+                autoCorrect={ false }
+              />
+              <TouchableWithoutFeedback onPress={ this.togglePw }>
+                <View style={ styles.icon } >
+                  <PwIcon
+                    color={ Colors.ORANGE }
+                    width={ 16 }
+                    height={ 16 }
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+
+            : <TextInput
+              { ...myProps }
+              { ...this.props }
+              ref={ reference }
+              underlineColorAndroid="rgba(0,0,0,0)"
+              autoCorrect={ false }
+            />
+        }
       </View>
     );
   }
@@ -64,39 +149,11 @@ VSInput.propTypes = {
   index: PropTypes.number,
   info: PropTypes.bool,
   toggle: PropTypes.func,
+  reference: PropTypes.func,
+  password: PropTypes.bool,
 };
 
 VSInput.defaultProps = {
   form: [],
   index: 0,
 };
-
-const styles = StyleSheet.create({
-  labelConainer: {
-    flexDirection: 'row',
-    width: '100%',
-    marginTop: 8,
-  },
-  label: {
-    fontSize: 12,
-  },
-  errLabel: {
-    color: '#E74C3C',
-    fontSize: 12,
-  },
-  input: {
-    width: '100%',
-    height: 48,
-    borderWidth: 1,
-    borderRadius: 4,
-    marginBottom: 8,
-    marginTop: 8,
-    padding: 8,
-  },
-  inputNorm: {
-    borderColor: '#D9DADB',
-  },
-  inputWarn: {
-    borderColor: '#E74C3C',
-  },
-});
