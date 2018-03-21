965a8edabfbeb7c4ece4da26b7d06ba72e739c35
Heraclio Rios <code@heracl.io>
2018-03-21 15:02:09 -0600

    Fix: Adding my two cents on the component !fix

--- packages/VSNumberCompare/index.js
+++ packages/VSNumberCompare/index.js
@@ -7,7 +7,76 @@ const DEFAULT_FONT_SIZE = 72;
 const FONT_RATIO = 4;
 const MINIMUM_SMALL_FONT_SIZE = 16;
 
-const VSNumberCompare = ({
+function buildValues(values, contrast, styles) {
+  const numbers = [];
+
+  values.forEach((value) => {
+    const commonStyles = [styles.value];
+
+    if (contrast) {
+      commonStyles.push(styles.contrast);
+
+      if (!value.color) {
+        value.color = 'white';
+      }
+    }
+
+
+    const valueString = value.value > ZERO_PAD_LIMIT ? `${value.value}` : `0${value.value}`;
+
+    numbers.push(<Text key={`${value.unit}`} style={[...commonStyles, { color: value.color }]}>{valueString}</Text>);
+    numbers.push(<Text key={`${value.unit}:`} style={[...commonStyles, styles.numberSpacing]}>{':'}</Text>);
+
+    return value;
+  });
+
+  numbers.pop();
+
+  return numbers;
+}
+
+function buildLegend(values, contrast, styles) {
+  const legend = [];
+
+  values.forEach((value) => {
+    const commonStyles = [styles.unit];
+
+    if (contrast) {
+      commonStyles.push(styles.contrast);
+    }
+
+    legend.push(<Text key={`${value.unit}`} style={[...commonStyles]}>{value.unit.toUpperCase()}</Text>);
+    legend.push(<Text key={`${value.unit}:`} style={[...commonStyles, styles.legendSpacing]}>{':'}</Text>);
+
+    return value;
+  });
+
+  legend.pop();
+
+  return legend;
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  contrast: {
+    color: 'white',
+  },
+  comparison: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  legendBottom: {},
+  legendTop: {
+    top: -2,
+    marginBottom: 8,
+  },
+});
+
+const UI = ({
   title = 'Defaults',
   values = [
     {
@@ -28,120 +97,55 @@ const VSNumberCompare = ({
 }) => {
   const SMALL_FONT = fontSize / fontRatio > fontMinimum ? fontSize / fontRatio : fontMinimum;
 
-  const styles = StyleSheet.create({
-    main: {
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-    },
-    contrast: {
-      color: 'white',
-    },
-    comparison: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    legendBottom: {},
-    legendTop: {
-      top: -2,
-      marginBottom: 8,
-    },
-    value: {
-      fontSize,
-      fontFamily: fontMain,
-    },
-    unit: {
-      fontSize: SMALL_FONT,
-      fontFamily: fontSecondary,
-    },
-    title: {
-      fontSize: SMALL_FONT,
-      fontFamily: fontMain,
-      marginBottom: 12,
-    },
-    numberSpacing: {
-      paddingLeft: numberSpacing,
-      paddingRight: numberSpacing,
-    },
-    legendSpacing: {
-      paddingLeft: legendSpacing,
-      paddingRight: legendSpacing,
-    },
-  });
+  styles.value = {
+    fontSize,
+    fontFamily: fontMain,
+  };
+
+  styles.unit = {
+    fontSize: SMALL_FONT,
+    fontFamily: fontSecondary,
+  };
+
+  styles.title = {
+    fontSize: SMALL_FONT,
+    fontFamily: fontMain,
+    marginBottom: 12,
+  };
+
+  styles.numberSpacing = {
+    paddingLeft: numberSpacing,
+    paddingRight: numberSpacing,
+  };
+
+  styles.legendSpacing = {
+    paddingLeft: legendSpacing,
+    paddingRight: legendSpacing,
+  };
 
   return (
     <View style={ styles.main }>
-      <Text style={ contrast ? [ styles.title, styles.contrast ] : styles.title }>{ title.toUpperCase() }</Text>
+      <Text style={ contrast ? [ styles.title, styles.contrast ] : styles.title }>{title.toUpperCase()}</Text>
       {
         legendPosition === 'top' &&
         <View style={ [ styles.comparison, styles.legendTop ] }>
-          { buildLegend(values, contrast, styles) }
+          {buildLegend(values, contrast, styles)}
         </View>
       }
       <View style={ styles.comparison }>
-        { buildValues(values, contrast, styles) }
+        {buildValues(values, contrast, styles)}
       </View>
       {
         legendPosition === 'bottom' &&
         <View style={ [ styles.comparison, styles.legendBottom ] }>
-          { buildLegend(values, contrast, styles) }
+          {buildLegend(values, contrast, styles)}
         </View>
       }
     </View>
   );
 };
 
-function buildValues(values, contrast, styles) {
-  const numbers = [];
-
-  values.forEach((value) => {
-    const commonStyles = [ styles.value ];
-
-    if (contrast) {
-      commonStyles.push(styles.contrast);
-      if (!value.color) {
-        value.color = 'white';
-      }
-    }
-
-
-    const valueString = value.value > ZERO_PAD_LIMIT ? `${ value.value }` : `0${ value.value }`;
-
-    numbers.push(<Text key={ `${ value.unit }` } style={ [ ...commonStyles, { color: value.color } ] }>{ valueString }</Text>);
-    numbers.push(<Text key={ `${ value.unit }:` } style={ [ ...commonStyles, styles.numberSpacing ] }>{ ':' }</Text>);
-
-    return value; // Make linter happy
-  });
-
-  // Remove trailing colon
-  numbers.pop();
-
-  return numbers;
-}
-
-function buildLegend(values, contrast, styles) {
-  const legend = [];
-
-  values.forEach((value) => {
-    const commonStyles = [ styles.unit ];
-
-    if (contrast) {
-      commonStyles.push(styles.contrast);
-    }
-
-    legend.push(<Text key={ `${ value.unit }` } style={ [ ...commonStyles ] }>{ value.unit.toUpperCase() }</Text>);
-    legend.push(<Text key={ `${ value.unit }:` } style={ [ ...commonStyles, styles.legendSpacing ] }>{ ':' }</Text>);
-
-    return value; // Make linter happy
-  });
-
-  // Remove trailing colon
-  legend.pop();
-
-  return legend;
-}
-
-VSNumberCompare.propTypes = {
+UI.propTypes = {
   title: PropTypes.string,
   values: PropTypes.array,
   legendPosition: PropTypes.string,
@@ -155,4 +159,4 @@ VSNumberCompare.propTypes = {
   legendSpacing: PropTypes.number,
 };
 
-export default VSNumberCompare;
+export default UI;
