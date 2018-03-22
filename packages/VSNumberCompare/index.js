b81c1028062c42c3b0400f5db13b381323499745
Tanner Christensen <devtanc@gmail.com>
2018-03-21 19:46:59 -0600

    fix: Refactor
    fix: Update readme

--- packages/VSNumberCompare/index.js
+++ packages/VSNumberCompare/index.js
@@ -1,158 +1,107 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, Text, StyleSheet } from 'react-native'
 
-const ZERO_PAD_LIMIT = 9;
-const DEFAULT_FONT_SIZE = 72;
-const FONT_RATIO = 4;
-const MINIMUM_SMALL_FONT_SIZE = 16;
+const defaultStyles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  legend: {
+    fontSize: 14,
+    fontFamily: 'TradeGothicThin',
+    paddingLeft: 2,
+    paddingRight: 2,
+  },
+  number: {
+    fontSize: 72,
+    fontFamily: 'TradeGothicBold',
+    paddingLeft: 1,
+    paddingRight: 1,
+    paddingTop: 1,
+  },
+  legendTop: {
+    top: -2,
+    marginBottom: 8,
+  },
+})
 
-const VSNumberCompare = ({
-  title = 'Defaults',
-  values = [
+const Wrapper = ({
+  data = [
     {
       unit: 'defs',
       value: 0,
       color: 'black',
     },
   ],
-  legendPosition = 'bottom',
-  contrast = false,
-  fontMain = 'TradeGothicBold',
-  fontSecondary = 'TradeGothicThin',
-  fontSize = DEFAULT_FONT_SIZE,
-  fontRatio = FONT_RATIO,
-  fontMinimum = MINIMUM_SMALL_FONT_SIZE,
-  numberSpacing = 1,
-  legendSpacing = 2,
-}) => {
-  const SMALL_FONT = fontSize / fontRatio > fontMinimum ? fontSize / fontRatio : fontMinimum;
+  style = null,
+  children,
+}) => (
+  <View style={[ defaultStyles.wrapper, style ]}>
+    {React.Children.map(children, child => React.cloneElement(child, { data }))}
+  </View>
+)
 
-  const styles = StyleSheet.create({
-    main: {
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-    },
-    contrast: {
-      color: 'white',
-    },
-    comparison: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    legendBottom: {},
-    legendTop: {
-      top: -2,
-      marginBottom: 8,
-    },
-    value: {
-      fontSize,
-      fontFamily: fontMain,
-    },
-    unit: {
-      fontSize: SMALL_FONT,
-      fontFamily: fontSecondary,
-    },
-    title: {
-      fontSize: SMALL_FONT,
-      fontFamily: fontMain,
-      marginBottom: 12,
-    },
-    numberSpacing: {
-      paddingLeft: numberSpacing,
-      paddingRight: numberSpacing,
-    },
-    legendSpacing: {
-      paddingLeft: legendSpacing,
-      paddingRight: legendSpacing,
-    },
-  });
-
-  return (
-    <View style={ styles.main }>
-      <Text style={ contrast ? [ styles.title, styles.contrast ] : styles.title }>{ title.toUpperCase() }</Text>
-      {
-        legendPosition === 'top' &&
-        <View style={ [ styles.comparison, styles.legendTop ] }>
-          { buildLegend(values, contrast, styles) }
-        </View>
-      }
-      <View style={ styles.comparison }>
-        { buildValues(values, contrast, styles) }
-      </View>
-      {
-        legendPosition === 'bottom' &&
-        <View style={ [ styles.comparison, styles.legendBottom ] }>
-          { buildLegend(values, contrast, styles) }
-        </View>
-      }
-    </View>
-  );
-};
-
-function buildValues(values, contrast, styles) {
-  const numbers = [];
-
-  values.forEach((value) => {
-    const commonStyles = [ styles.value ];
-
-    if (contrast) {
-      commonStyles.push(styles.contrast);
-      if (!value.color) {
-        value.color = 'white';
-      }
-    }
-
-
-    const valueString = value.value > ZERO_PAD_LIMIT ? `${ value.value }` : `0${ value.value }`;
-
-    numbers.push(<Text key={ `${ value.unit }` } style={ [ ...commonStyles, { color: value.color } ] }>{ valueString }</Text>);
-    numbers.push(<Text key={ `${ value.unit }:` } style={ [ ...commonStyles, styles.numberSpacing ] }>{ ':' }</Text>);
-
-    return value; // Make linter happy
-  });
-
-  // Remove trailing colon
-  numbers.pop();
-
-  return numbers;
+Wrapper.propTypes = {
+  data: PropTypes.array,
+  style: PropTypes.any,
+  children: PropTypes.any,
 }
 
-function buildLegend(values, contrast, styles) {
-  const legend = [];
-
-  values.forEach((value) => {
-    const commonStyles = [ styles.unit ];
-
-    if (contrast) {
-      commonStyles.push(styles.contrast);
+const Numbers = ({
+  data,
+  style,
+}) => (
+  <View style={defaultStyles.row}>
+    {
+      data.reduce((arr, item, index) => ([
+        ...arr,
+        <Text key={item.unit} style={[ defaultStyles.number, style, { color: item.color } ]}>{ numberPad(item.value) }</Text>,
+        index < data.length - 1 && <Text key={`${item.unit}:`} style={[ defaultStyles.number, style ]}>{ ':' }</Text>,
+      ]), [])
     }
+  </View>
+)
 
-    legend.push(<Text key={ `${ value.unit }` } style={ [ ...commonStyles ] }>{ value.unit.toUpperCase() }</Text>);
-    legend.push(<Text key={ `${ value.unit }:` } style={ [ ...commonStyles, styles.legendSpacing ] }>{ ':' }</Text>);
-
-    return value; // Make linter happy
-  });
-
-  // Remove trailing colon
-  legend.pop();
-
-  return legend;
+Numbers.propTypes = {
+  data: PropTypes.array,
+  style: PropTypes.any,
 }
 
-VSNumberCompare.propTypes = {
-  title: PropTypes.string,
-  values: PropTypes.array,
-  legendPosition: PropTypes.string,
-  contrast: PropTypes.bool,
-  fontMain: PropTypes.string,
-  fontSecondary: PropTypes.string,
-  fontSize: PropTypes.number,
-  fontRatio: PropTypes.number,
-  fontMinimum: PropTypes.number,
-  numberSpacing: PropTypes.number,
-  legendSpacing: PropTypes.number,
-};
+const Legend = ({
+  data,
+  style,
+}) => (
+  <View style={defaultStyles.row}>
+    {
+      data.reduce((arr, item, index) => ([
+        ...arr,
+        <Text key={item.unit} style={[ defaultStyles.legend, style ]}>{ item.unit.toUpperCase() }</Text>,
+        index < data.length - 1 && <Text key={`${item.unit}:`} style={[ defaultStyles.legend, style ]}>{ ':' }</Text>,
+      ]), [])
+    }
+  </View>
+)
 
-export default VSNumberCompare;
+Legend.propTypes = {
+  data: PropTypes.array,
+  style: PropTypes.any,
+}
+
+function numberPad(number) {
+  if (number < 10) {
+    return `0${ number }`
+  }
+  return `${ number }`
+}
+
+export default {
+  Wrapper,
+  Numbers,
+  Legend,
+}
